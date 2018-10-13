c0e5c738c8bc9633b79757c3e98cdc5d68e9b624
Brian <bjlp7c@mail.missouri.edu>
2018-10-13 11:18:04 -0500

    First Commit

---
+++ Tiger-Hacks-Heart-Rate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tiger-Hacks-Heart-Rate
+//
+//  Created by Lasker, Brian (MU-Student) on 10/13/18.
+//  Copyright © 2018 Lasker, Brian (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
