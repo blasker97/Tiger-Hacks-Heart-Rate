9896407f59f989d60ae684372fcd5cd81fdcbc71
Brian <bjlp7c@mail.missouri.edu>
2018-10-13 11:27:11 -0500

    First Commit

---
+++ Tiger-Hacks-Heart-Rate/Tiger-Hacks-Heart-Rate-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Tiger-Hacks-Heart-Rate-Bridging-Header.h
+//  Tiger-Hacks-Heart-Rate
+//
+//  Created by Lasker, Brian (MU-Student) on 10/13/18.
+//  Copyright © 2018 Lasker, Brian (MU-Student). All rights reserved.
+//
+
+#ifndef Tiger_Hacks_Heart_Rate_Bridging_Header_h
+#define Tiger_Hacks_Heart_Rate_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Tiger_Hacks_Heart_Rate_Bridging_Header_h */
